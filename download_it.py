8008a2fedf7e1aa517143138d7d68dd06756bd5b
WhatsEmo <bscheng@uci.edu>
2016-02-25 20:02:12 -0800

    Added Enroll Function
    Program goes into the Enrollment Menu, but can't do much yet.

--- download_it.py
+++ download_it.py
@@ -6,9 +6,23 @@ def loginIn(username, password):
     browser.visit('https://www.reg.uci.edu/cgi-bin/webreg-redirect.sh')
     browser.fill('ucinetid', username)
     browser.fill('password', password)
+    browser.find_by_name('login_button').click()
+    return browser
+
+
+def enroll(browser, listofClasses):
+    if("UCInetID Secure Web Login" ==  browser.title):
+        print("Invalid Login")
+        browser.quit()
+    else:
+        print("Success")
+        browser.find_by_name('Enrollment Menu').click()
+        for courseID in listofClasses:
+            browser.fill("<################>", courseID)
+            browser.find_by_name('<###############>').click()
 
 
 
+b = loginIn('username','password')
 
-
-loginIn('zxc','zxc')
+enroll(b)
